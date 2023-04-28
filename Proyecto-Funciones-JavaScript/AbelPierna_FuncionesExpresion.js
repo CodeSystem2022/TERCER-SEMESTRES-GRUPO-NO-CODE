880596c3f9f02951b054081bb6921268ab15ca58
Abelino537 <103205401+Abelino537@users.noreply.github.com>
2023-04-28 17:04:40 -0300

    Abel Pierna 1.3
    Subo el codigo de lo que seria un funcion de tipo expresión

---
+++ Proyecto-Funciones-JavaScript/AbelPierna_FuncionesExpresion.js
@@ -0,0 +1,14 @@
+/*
+Laboratorio III
+UTN
+Clase 3
+10/04/23
+Abel Pierna
+Funciones
+*/
+
+//Funciones de tipo expresión
+
+let variableExpresion = function (a , b){return a + b}; //se escribe todo en una linea y termina en ; 
+let resultado2 = variableExpresion(20 , 5);
+console.log("El resultado 2 es: " + resultado2);
