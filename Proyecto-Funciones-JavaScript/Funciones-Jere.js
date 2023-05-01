a76f7908affc6249194f33fee2b4924697ea2492
Jeremias Riquero <111074834+JereRiquero@users.noreply.github.com>
2023-05-01 10:52:02 -0300

    Arguments
    Aca esta subido mi trabajo de js

---
+++ Proyecto-Funciones-JavaScript/Funciones-Jere.js
@@ -0,0 +1,6 @@
+//1.7 Parametros y argumentos, uso de "arguments" (Jeremias Riquero)
+
+function argumentosParametros(a, b){
+    return console.log("El argumento en el indice 1 es : " + arguments[1]);
+}
+argumentosParametros(2 , 6);
