c64fdbdd423d37fa408603abb0e7ccf36c0f65c6
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-05-06 19:33:00 -0300

    2do commit

--- Proyecto-Objetos-JavaScript/js(Andres-Ezequiel).js
+++ Proyecto-Objetos-JavaScript/js(Andres-Ezequiel).js
@@ -1,11 +1,24 @@
 const formulario = document.getElementById("form1");
 
-formulario-addEventListener("submit", function(event) {
-    event.preventDefault(); //Previene el comportamiento predet de form
-
+function formulario1() {
     let nombre = this.document.getElementById("dato1_1").value;
     let apellido = this.document.getElementById("dato1_2").value;
     let edad = this.getElementById("dato1_3").value;
 
-    alert("Formulario enviado correctamente");
-});
+    let persona1 = {
+        nombre: nombre,
+        apellido: apellido,
+        edad: edad,
+        mostrarConfirmacion: function () {
+            alert("Los datos han sido recibidos correctamente")
+        },
+        rellenarDatos: function () {
+            document.getElementById('devolucionTitulo').innerHTML = "Datos de " + nombre + " " + apellido + ": ";
+            document.getElementById('devolucionNombre').innerHTML = 'Nombre: ' + nombre;
+            document.getElementById('devolucionApellido').innerHTML = 'Apellido: ' + apellido;
+        }
+
+}
+persona1.mostrarConfirmacion();
+persona1.rellenarDatos();
+}
