e9a33893d84ff4e5cf69488d2c9008873c4bb7ff
Andres Winckler <andreswinckler@gmail.com>
2023-05-06 22:37:44 -0300

    usando New Object y agregando campos faltantes

--- Proyecto-Objetos-JavaScript/js(Andres-Ezequiel).js
+++ Proyecto-Objetos-JavaScript/js(Andres-Ezequiel).js
@@ -1,20 +1,23 @@
 const formulario1 = document.getElementById("form1")
 
+
+alert(confirma)
 function form1(){
-    let nombre = document.getElementById("dato1_1").value;
-    let apellido = document.getElementById("dato1_2").value;
-    let edad = document.getElementById("dato1_3").value;
-
-    let persona1 = {
-        nombre: nombre,
-        apellido: apellido,
-        edad: edad,
-        mostrarConfirmacion: function () {
-            alert("Los datos han sido recibidos correctamente")
-        },
-
+    let persona1 = new Object();
+    persona1.nombre = document.getElementById("dato1_1").value;
+    persona1.apellido = document.getElementById("dato1_2").value;
+    persona1.edad = document.getElementById("dato1_3").value;
+    persona1.dni = document.getElementById("dato1_4").value;
+    persona1.enfermedad = document.getElementById("dato1_5").value;
+    persona1.gruposanguineo = document.getElementById("dato1_6").value;
+    // let persona1 = {
+    //     nombre: nombre,
+    //     apellido: apellido,
+    //     edad: edad,
+    alert('datos recibidos correctamente')
+    alert('nombre:'+ persona1.nombre+'\napellido:'+persona1.apellido+'\nedad:'+persona1.edad+'\ndni:'+persona1.dni+'\nenfermedad:'+persona1.enfermedad+'\nGrupo Sanguineo'+persona1.gruposanguineo)
 }
-persona1.mostrarConfirmacion();
-alert('nombre:'+ nombre+' apellido:'+apellido+' edad:'+edad)
 
-}
+
+
+
