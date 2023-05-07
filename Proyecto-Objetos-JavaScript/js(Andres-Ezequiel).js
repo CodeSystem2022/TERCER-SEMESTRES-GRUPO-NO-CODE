1c99dd5ad1af2f028e78c430ee13d53840f99a8c
Andres Winckler <andreswinckler@gmail.com>
2023-05-06 23:01:33 -0300

    resubiendo

--- Proyecto-Objetos-JavaScript/js(Andres-Ezequiel).js
+++ Proyecto-Objetos-JavaScript/js(Andres-Ezequiel).js
@@ -10,10 +10,6 @@ function form1(){
     persona1.dni = document.getElementById("dato1_4").value;
     persona1.enfermedad = document.getElementById("dato1_5").value;
     persona1.gruposanguineo = document.getElementById("dato1_6").value;
-    // let persona1 = {
-    //     nombre: nombre,
-    //     apellido: apellido,
-    //     edad: edad,
     alert('datos recibidos correctamente')
     alert('nombre:'+ persona1.nombre+'\napellido:'+persona1.apellido+'\nedad:'+persona1.edad+'\ndni:'+persona1.dni+'\nenfermedad:'+persona1.enfermedad+'\nGrupo Sanguineo'+persona1.gruposanguineo)
 }
