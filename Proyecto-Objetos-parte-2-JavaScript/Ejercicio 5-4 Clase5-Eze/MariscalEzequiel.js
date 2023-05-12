628f45e220d357a173f12c493d40023b304cdd09
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-05-12 16:34:41 -0300

    Create MariscalEzequiel.js
    add_description

---
+++ Proyecto-Objetos-parte-2-JavaScript/Ejercicio 5-4 Clase5-Eze/MariscalEzequiel.js
@@ -0,0 +1,13 @@
+
+// Distintas formas de crear objetos - Mariscal Ezequiel
+
+/* 
+Ver el video 5.4 de la clase 5 y desarrollar un código simple aplicando lo enseñado. 
+Solo hacer las siguientes formas:
+
+Caso Boolean 1 y 2
+Caso Arreglo 1 y 2
+Caso Funciones 1 y 2
+
+*/
+
