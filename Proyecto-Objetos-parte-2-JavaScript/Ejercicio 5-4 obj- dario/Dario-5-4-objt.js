f277d681fd9bb42d95a1d771402b9b8785a3fcab
Dario Walter Carrizo <108595018+dwc1970@users.noreply.github.com>
2023-05-14 19:15:30 -0300

    Create Dario-5-4-objt.js

---
+++ Proyecto-Objetos-parte-2-JavaScript/Ejercicio 5-4 obj- dario/Dario-5-4-objt.js
@@ -0,0 +1,19 @@
+//Diferetes formas de crear objetos 
+// Caso N-1
+let miObjeto1 = new Object(); // Esta es la opción formal
+
+//Caso N-2
+let miObjeto2 = {}; //Esta opción es breve y recomendada
+
+//Caso String 1
+let miCadena1 = new String('Panchuque'); // Sintaxis formal
+
+//Caso String 2
+let miCadena2 = 'Panchuque'; // Esta es la clase simplificada y recomendada
+
+//Caso con números 1
+let miNumero1 = new Number(1); // Es Formal no recomendable.
+
+//Caso con números 2 
+let miNumero2 = 1; //Sintaxis recomendada
+
