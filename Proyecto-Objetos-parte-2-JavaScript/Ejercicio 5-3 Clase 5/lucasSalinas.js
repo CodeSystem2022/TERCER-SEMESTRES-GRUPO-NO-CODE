37c2da3320ff72b8737924722234de12aa123d84
LucasSalinas <111472464+LucasSalinas@users.noreply.github.com>
2023-05-15 01:06:20 -0700

    Update lucasSalinas.js

--- Proyecto-Objetos-parte-2-JavaScript/Ejercicio 5-3 Clase 5/lucasSalinas.js
+++ Proyecto-Objetos-parte-2-JavaScript/Ejercicio 5-3 Clase 5/lucasSalinas.js
@@ -1,13 +1,17 @@
 /*Clase 5 JS: Ejercicio 5-3
 Lucas Salinas
-Uso de constructores en JS
-*/ 
+Uso de constructores en JS y metodos dentro del constructor
+*/
 function Persona(nombre, apellido, edad = "20", email, lang){
     this.nombre = nombre;
     this.apellido = apellido;
     this.edad = edad;
     this.email = email;
     this.lenguaje = lang.toUpperCase();
+    this.nombreCompleto = function(){
+        return this.nombre+" "+this.apellido;   
+    }
 }
 let persona1 = new Persona("Lucas", "Salinas", this.edad, "lsaf.serviciotecnico@gmail.com", "es");
 console.log(persona1);
+console.log(persona1.nombreCompleto());
