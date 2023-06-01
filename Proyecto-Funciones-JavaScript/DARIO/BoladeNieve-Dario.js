3933639c95634e4128abdd6516421c470c89a1aa
Dario Walter Carrizo <108595018+dwc1970@users.noreply.github.com>
2023-06-01 00:06:21 -0300

    Create BoladeNieve-Dario.js
    Se sube  Bola de Nieve ..

---
+++ Proyecto-Funciones-JavaScript/DARIO/BoladeNieve-Dario.js
@@ -0,0 +1,16 @@
+class Gato {
+    constructor(nombre, edad, color) {  // se define la clase gato, con el constructor y el metodo "dormir"
+      this.nombre = nombre;
+      this.edad = edad;
+      this.color = color;
+    }
+  
+    dormir() {
+      console.log("BolaDeNieve10 ha llegado");
+    }
+  }
+   // se crea la instancia de la clase "gato" en esta instancia especificamos que la clase tiene sus propios valores 
+  const BolaDeNieve9 = new Gato("BolaDeNieve9", 4, "gris"); // argumentos del constructor 
+  BolaDeNieve9.dormir();
+  
+  // Dario Walter Carrizo </>
