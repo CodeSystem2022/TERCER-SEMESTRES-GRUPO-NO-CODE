786743053ad0d6b9dcf13ca47b0ac5b63abba1b9
LucasSalinas <111472464+LucasSalinas@users.noreply.github.com>
2023-06-06 19:07:27 -0700

    Add files via upload

---
+++ Proyecto-Clases-Parte2-JavaScript/Muebles.js
@@ -0,0 +1,28 @@
+/*Clase 7: Clases en JS:
+Herencia de métodos, sobreescritura, clase Object, toString y polimorfismo
+Grupo: NoCode*/
+
+/* 1- DANTE y EZEQUIEL:
+- Crear la clase padre “Mueble” junto con su método constructor.
+- Sus parámetros serán “material” y “color”.
+- Agregar getters y setters de la clase padre.
+*/
+
+/* 2- ABEL Y FRANCO:
+- Agregar un método que retorne el material y el color del mueble. Lo llamaras “mostrarMueble”.
+- Crear el método “toString” de la clase padre para retornar el método “mostrarMueble”.
+- Crear 2 objetos de la clase padre e imprimir con el método “toString”.
+*/
+
+/* 3- ANDRES Y DARIO:
+- Crear una clase hija “TipoDeMueble” de la clase “Mueble”:
+- Agregar método constructor.
+- Hacer dos nuevos parámetros: “tipo” (sillón, cama, silla, etc) y “uso” (para sentarse, para guardar cosas, para dormir, etc).
+- En la clase hija crear getters y setters para “tipo” y “uso”.
+*/
+
+/* 4- LUCAS Y JERE:
+- Sobrescribir el método “mostrarMueble” en la clase hija, agregando el tipo de mueble y su uso.
+- Crear 2 objetos de la clase hija y mostrar con el método “toString”.
+- Dejar claro el uso del polimorfismo imprimiendo el mismo método “toString” desde distintos objetos.
+*/
